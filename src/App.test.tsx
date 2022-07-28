062cfa8b8cab8eb125e2176dae6a55b19ca7f0d5
Ela Jahoda <3la.jah0da@gmail.com>
2022-07-28 12:52:19 +0200

    remove unnecessary files from the template

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import JsHistory from './JS-history/JsHistory'
-import React from 'react'
-
-test('renders learn react link', () => {
-  render(<JsHistory />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import './index.css'
 import JsHistory from './JS-history/JsHistory'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -11,8 +9,3 @@ root.render(
     <JsHistory />
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
