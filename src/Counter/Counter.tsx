76bf03de18633bc063721189e63b52681ced4a71
Ela Jahoda <3la.jah0da@gmail.com>
2022-07-31 20:49:55 +0200

    delete unnecessary comment - Hook version of Counter

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -23,8 +23,7 @@ export class Counter extends React.Component<Props, State> {
             this.setState(prevstate => ({ counter: prevstate.counter - 1 }))
           }}
         >
-          {' '}
-          -{' '}
+          -
         </button>
         <span> {this.state.counter} </span>
         <button
@@ -32,30 +31,9 @@ export class Counter extends React.Component<Props, State> {
             this.setState(prevstate => ({ counter: prevstate.counter + 1 }))
           }}
         >
-          {' '}
-          +{' '}
+          +
         </button>
       </Div_Styled>
     )
   }
 }
-
-// Counter - version with Hook:
-// export const Counter = () => {
-//   const [count, setCount] = useState(0)
-
-//   const decrementCount = () => {
-//     setCount(prevCount => prevCount - 1)
-//   }
-//   const incrementCount = () => {
-//     setCount(prevCount => prevCount + 1)
-//   }
-//   return (
-//     <P>
-//       <h1>Counter App</h1>
-//       <button onClick={decrementCount}> - </button>
-//       <span> {count} </span>
-//       <button onClick={incrementCount}> + </button>
-//     </P>
-//   )
-// }
