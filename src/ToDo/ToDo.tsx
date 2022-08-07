1f3aaf2d30d2484101aa982716548a5cf0c9e33b
Ela Jahoda <3la.jah0da@gmail.com>
2022-08-07 14:07:49 +0200

    update after code review

--- src/ToDo/ToDo.tsx
+++ src/ToDo/ToDo.tsx
@@ -1,57 +1,86 @@
 import { Div_Styled } from '../HomePage'
 import { theme } from '../theme'
-import React, { ChangeEvent, FC, useState } from 'react'
+import { themeTodo } from './theme'
+import { uniqueId } from '../util'
+import React, { ChangeEvent, useState } from 'react'
 import styled from '@emotion/styled'
 
-type TTask = {
+type Task = {
   taskName: string
   id: number
   complete: boolean
 }
 
-const lsId = {
+const localStorageId = {
   tasks: ' tasks:list',
 }
 
-const getTasksLS = (): TTask[] => {
-  const lsTasks = localStorage.getItem(lsId.tasks)
-  if (lsTasks) {
-    return JSON.parse(lsTasks)
+const getTaskLocalStorage = (): Task[] => {
+  const localStorageTasks = localStorage.getItem(localStorageId.tasks)
+  if (localStorageTasks) {
+    return JSON.parse(localStorageTasks)
   }
   return []
 }
-export const ToDo: FC = () => {
-  const [todoList, _setTodoList] = useState(getTasksLS())
-  const [task, setTask] = useState<string>('')
 
-  const setTodoList = (items: TTask[]) => {
-    localStorage.setItem(lsId.tasks, JSON.stringify(items))
+function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') {
+      return initialValue
+    }
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value
+      setStoredValue(valueToStore)
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return [storedValue, setValue] as const
+}
+
+export const ToDo = () => {
+  const [todoList, _setTodoList] = useState(getTaskLocalStorage())
+  const [task, setTask] = useState('')
+  const [name, setName] = useLocalStorage<string | Task[]>(localStorageId.tasks, task)
+  const [error, setError] = useState<boolean>(false)
+
+  const setTodoList = (items: Task[]) => {
     _setTodoList(items)
-  }
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setTask(event.target.value)
+    setName(items)
   }
 
-  const addTask = (): void => {
-    if (task === '') {
-      alert('Please, add task item.')
+  const addTask = () => {
+    if (task.trim() === '') {
+      const newError = true
+      setError(newError)
       return
     }
-    const newTask = { taskName: task, id: Math.floor(Math.random() * 10000), complete: false }
+    const newTask = { taskName: task, id: uniqueId(), complete: false }
 
     setTodoList([newTask, ...todoList])
+    console.log(localStorageId.tasks)
+
     setTask('')
+    setError(false)
   }
 
-  const deleteTask = (taskNameToDelete: string): void => {
-    setTodoList(
-      todoList.filter(task => {
-        return task.taskName !== taskNameToDelete
-      })
-    )
+  const deleteTask = (taskNameToDelete: string) => {
+    setTodoList(todoList.filter(task => task.taskName !== taskNameToDelete))
   }
 
-  const AllCheckboxCh = (event: ChangeEvent<HTMLInputElement>) => {
+  const AllCheckboxChecked = (event: ChangeEvent<HTMLInputElement>) => {
     let checked = event.target.checked
     setTodoList(
       todoList.map(data => {
@@ -61,102 +90,100 @@ export const ToDo: FC = () => {
     )
   }
 
-  const getToDosLeft = (): number => {
-    return todoList.filter(todo => !todo.complete).length
-  }
+  const getToDosLeft = todoList.filter(todo => !todo.complete).length
 
   return (
     <Div_Styled>
-      <h1>Todo app</h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-        }}
-      >
-        <Div_TodoApp>
-          <Div_header>
-            <Input_styled
-              type='text'
-              placeholder='What needs to be dome?'
-              onChange={handleChange}
-              value={task}
-            />
-            <Button_styled onClick={addTask}>Add</Button_styled>
-          </Div_header>
-          <table>
-            <thead>
-              <tr>
-                <th>
-                  <input type='checkbox' onChange={AllCheckboxCh} />
-                </th>
-                <th>List of items:</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {todoList.map(task => {
-                return (
-                  <tr key={task.id}>
-                    <Td_styled width='5%'>
-                      <input
-                        onChange={event => {
-                          let checked = event.target.checked
-                          setTodoList(
-                            todoList.map(data => {
-                              if (task.id === data.id) {
-                                data.complete = checked
-                              }
-                              return data
-                            })
-                          )
-                        }}
-                        value={task.taskName}
-                        type='checkbox'
-                        checked={task.complete}
-                      />
-                    </Td_styled>
-                    <Td_styled width='100%' border='solid 1px #d3d3d3'>
-                      <Span_styled checked={task.complete}>{task.taskName}</Span_styled>
-                    </Td_styled>
-                    <Td_styled width='10%'>
-                      <Button_styled
-                        onClick={() => {
-                          deleteTask(task.taskName)
-                        }}
-                      >
-                        X
-                      </Button_styled>
-                    </Td_styled>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-          <Div_Counter>{getToDosLeft()} items left</Div_Counter>
-        </Div_TodoApp>
-      </form>
+      <h1>Todo App</h1>
+      <Div_TodoApp>
+        <Div_header>
+          <Input_styled
+            type='text'
+            placeholder={
+              error ? 'Error: Nothing added. Please, add task item...' : 'What need to be done?'
+            }
+            error={error}
+            onChange={e => {
+              setTask(e.target.value)
+            }}
+            value={task}
+          />
+          <Button_styled onClick={addTask}>Add</Button_styled>
+        </Div_header>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <input type='checkbox' onChange={AllCheckboxChecked} />
+              </th>
+              <th>List of items:</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {todoList.map(task => {
+              return (
+                <tr key={task.id}>
+                  <Td_styled width='5%'>
+                    <input
+                      onChange={event => {
+                        let checked = event.target.checked
+                        setTodoList(
+                          todoList.map(data => {
+                            console.log(data)
+                            const data1 = { ...data }
+                            if (task.id === data1.id) {
+                              data1.complete = checked
+
+                              console.log(data.complete === data1.complete)
+                              console.log(task.id === data1.id)
+                            }
+                            console.log(data.complete === data1.complete)
+                            return data1
+                          })
+                        )
+                      }}
+                      value={task.taskName}
+                      type='checkbox'
+                      checked={task.complete}
+                    />
+                  </Td_styled>
+                  <Td_styled width='100%' border='solid 1px #d3d3d3'>
+                    <Span_styled checked={task.complete}>{task.taskName}</Span_styled>
+                  </Td_styled>
+                  <Td_styled width='10%'>
+                    <Button_styled onClick={() => deleteTask(task.taskName)}>X</Button_styled>
+                  </Td_styled>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <Div_Counter>{getToDosLeft} items left</Div_Counter>
+      </Div_TodoApp>
     </Div_Styled>
   )
 }
 
-const Input_styled = styled.input`
+const Input_styled = styled.input<{ error?: boolean }>`
   margin: auto;
   border: none;
   width: 80%;
   &:focus {
     outline: none;
   }
+  placeholder: ${props => (props.error ? 'abc' : 'def')};
 `
 
 const Div_TodoApp = styled.div`
   box-sizing: border-box;
   text-align: left;
-  border-bottom: solid 2px grey;
+  border-bottom: solid 2px ${themeTodo.primaryColor};
   border-radius: 5px;
   width: 50%;
   margin: auto;
   background: #fff;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: ${themeTodo.boxShadow};
 `
 
 const Td_styled = styled.td<{ width?: string; border?: string }>`
@@ -167,7 +194,6 @@ const Td_styled = styled.td<{ width?: string; border?: string }>`
 `
 
 const Div_Counter = styled.div`
-  color: black;
   font-size: small;
   padding: 5px;
   font-size: 15px;
@@ -181,7 +207,7 @@ const Span_styled = styled.span<{ checked: boolean }>`
 const Div_header = styled.div`
   display: flex;
   background: #fff;
-  border-bottom: solid 1px grey;
+  border-bottom: solid 1px ${themeTodo.primaryColor};
   padding: 5px;
   box-shadow: ${theme.boxShadow};
 `
---
+++ src/ToDo/theme.ts
@@ -0,0 +1,4 @@
+export const themeTodo = {
+  primaryColor: 'grey',
+  boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1)',
+}
