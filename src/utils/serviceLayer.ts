fc3fb1e087f8227e120f2ba4d316c8448acf11a1
Ela Jahoda <3la.jah0da@gmail.com>
2022-09-09 18:45:33 +0200

    added swagger documentation

--- src/utils/serviceLayer.ts
+++ src/utils/serviceLayer.ts
@@ -12,7 +12,7 @@ export const blogServices = {
   delete: async (slug: string) => {
     const response = (await serviceLayerFetch(apiBlog.detail(slug), {
       method: 'DELETE',
-    })) as {}
+    })) as Articles
   },
   update: async (slug: string, body: {}) => {
     const response = (await serviceLayerFetch(apiBlog.update(slug), {
